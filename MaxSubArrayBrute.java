40ef9e957a25a444b80e05d3c513cd2933797960
Sahil Sinnarkar <sahilsinnarkar@gmail.com>
2024-09-02 12:57:22 +0530

    max sub array sum brute force

--- MaxSubArrayBrute.java
+++ MaxSubArrayBrute.java
@@ -1,23 +1,28 @@
+import java.util.*;
+
 class MaxSubArrayBrute {
 
-    public static int maxSubArraySum(int arr[]){
+    public static int maxSubArraySum(int arr[], int startIndex, int endIndex){
         int maxSum = Integer.MIN_VALUE;
-        int n = arr.length;
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                int sum=0;
-                for(int k=i;k<j;k++){
-                    sum+=arr[k];
-                }
-                maxSum = Math.max(maxSum,sum);
+        for(int i=startIndex;i<=endIndex;i++){
+            int sum=0;
+            for (int j = i; j <= endIndex; j++) {
+                sum += arr[j];
+                maxSum = Math.max(maxSum, sum);
             }
         }
         return maxSum;
     }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        int maxSum = maxSubArraySum(arr);
+        System.out.print("Enter start index: ");
+        int startIndex = sc.nextInt();
+        System.out.print("Enter end index: ");
+        int endIndex = sc.nextInt();
+        int maxSum = maxSubArraySum(arr, startIndex, endIndex);
         System.out.println("Maximum sub array sum = " + maxSum);
+        sc.close();
     }
 }
